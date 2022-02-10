76d4c0e931c28800a36bf1ba0297d7f5f49c63e6
Pekka Alaluukas <Pekka.Alaluukas@mymail.com>
2022-02-10 14:18:27 +0200

    express basic esimerkki valmis

---
+++ express_test/app.js
@@ -0,0 +1,37 @@
+const express=require('express');
+
+const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.get('/',function(req,res){
+    res.send("Terve");
+});
+
+app.use(function(req,res,next){
+    console.log("Tarkistetaan oikeudet");
+    next();
+});
+
+app.get('/second',function(req,res){
+    res.send("Second endpoint");
+    console.log("Second endpoint");
+});
+
+app.get('/sayFname/:fname?',function(req,res){
+    if(req.params.fname) {
+        res.send("Terve "+req.params.fname);
+    }
+    else{
+        res.send("Terve nimetön");
+    }
+});
+
+app.post('/addName',function(req,res){
+    res.send(req.body);
+    console.log(req.body);
+});
+
+const port=3000;
+app.listen(3000);
